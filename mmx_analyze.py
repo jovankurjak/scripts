e9cece671998bdc9ba2990ade16f236183720e96
Jovan Kurjakov <Jovan.Kurjakov@rt-rk.com>
2018-04-11 13:11:25 +0200

    Improve parsing of percentages
    -Improve parsing of percentages of other functions
    -Improve regex(small improvement)
    -Code cleanup(removed a function)

--- mmx_analyze.py
+++ mmx_analyze.py
@@ -6,6 +6,7 @@ import argparse
 import os
 import re
 import csv
+import collections
 
 
 systemApplications = [
@@ -27,6 +28,10 @@ systemApplications = [
     'persistence',
     'systemservices'
 ]
+
+mydict = {}
+mylist = []
+
 
 # Use to get arguments if needed
 def getArgs():
@@ -61,42 +66,29 @@ def parseAll(file):
 
 # searches for a certain pattern inside the log file
 def searchForPattern(f, pattern):
-    myLine = None
-    with open(f) as book:
-        lines = book.readlines()
-        lastline = lines[-1]
-        for line in lines:
-            myLine = line
+    result = False
+    # with open(f, 'r', encoding='latin-1') as book:
+    with open(f, 'r') as book:
+        for line in book:
             line = line.rstrip()
             if re.search(pattern, line):
+                result = True
                 break
-
-        if myLine is lastline:
-            return False
-        else:
-            return True
-
-def index(filename, patterns):
-    with open(filename) as fp:
-        for line_number, line in enumerate(fp):
-            line = line.rstrip()
-            # Find first header
-            # if re.search('CPU=', line):
-            #     print(line_number.__str__() + " " + line)
-            for pat in patterns:
-                if re.search(pat, line):
-                    print(line_number.__str__() + " " + line)
+    return result
 
 
 # find headers in provided file
 def findHeader(file):
-    curHeaderStart = 0
-    curHeaderEnd = 0
+    """
+    This function goes through MMX log file and parses every
+    HB section for overall CPU consumption and CPU consumption of
+    currently active processes at that time
+    """
     tellPosition = 0
-    tellPositionEnd = 0
     headerCounter = 0
     csv_list = []
-    csvfile = '/home/rtrk/Desktop/cpu.csv'
+    # csvfile = '/home/rtrk/Desktop/cpu.csv'
+    csvfile = 'cpu_quick_analysis.csv'
     with open(file) as fp:
         fp.seek(0, 2)
         EOF = fp.tell()
@@ -118,17 +110,43 @@ def findHeader(file):
             parseForFunctions(fp, curHeaderStart, curHeaderEnd, csv_list)
             tellPosition = fp.tell()
 
-        print(headerCounter.__str__() + " " + tellPosition.__str__())
+        print('number of HB headers: ' + headerCounter.__str__() + " " + tellPosition.__str__())
 
         with open(csvfile, "w") as output:
             """Write the list to csv file."""
 
-            for entries in csv_list:
-                output.write(str(entries))
+            header = 'time,'
+            allvalues = ''
+            for key in mylist[-1].keys():
+                if key != 'time':
+                    header += (key + ',')
 
+            header += '\n'
+            for values in mylist[-4].values():
+                if mylist[-4]['time'] == values:
+                    allvalues = values + ',' + allvalues
+                else:
+                    allvalues += (values + ',')
 
-            # novi patern regex = r"\/([\w-]+)\s+\(\d+\)\s([\d.]+)%|(CPU)=(\d+)%"
-# patern za vreme time_regex = r"\b(\d+:\d+:\d+).\d\s+\|"
+            output.write(str(header))
+            # print(header)
+            # print(allvalues)
+            for entries in mylist:
+                currentline = ''
+                for key_up in mylist[-1].keys():
+                    for key in entries.keys():
+                        if key_up is key:
+                            if key is 'time':
+                                currentline = entries[key] + ',' + currentline
+                            else:
+                                currentline += (entries[key] + ',')
+                            break
+                    else:
+                        currentline += ('0' + ',')
+
+                # print(currentline)
+                currentline += '\n'
+                output.write(currentline)
 
 
 def getNextPattern(filePointer, lineNumber, pattern):
@@ -144,37 +162,46 @@ def getNextPattern(filePointer, lineNumber, pattern):
     return current_tell
 
 
-
 def parseForFunctions(filePointer, startPosition, endPosition, csv_list):
     filePointer.seek(startPosition + 1)
-
     for line_number, line in enumerate(iter(filePointer.readline, '')):
         if filePointer.tell() == endPosition:
             # End list row
             csv_list.append('\n')
+            od = collections.OrderedDict(sorted(mydict.items()))
+            mylist.append(od)
+            # print(od)
+            # print_od = ''
+            # for key in od:
+            #     print_od += '[' + key + ':' + od[key] + '%' + '] '
+            # print(print_od)
+            for value in mydict.values():
+                value = ''
             break
         line = line.rstrip()
         # check if header
         if re.search('CPU=', line) is not None:
-            regex = r"(\d\d:\d\d:\d\d)\.\d\s+\|[\s\w:]+T=\d+C,\s+CPU=(\d+)"
+            regex = r"(\d\d:\d\d:\d\d)[\s.\d]+\|[\s\w:]+T=\d+C,\s+CPU=(\d+)"
             reObj = re.search(regex, line)
             if reObj:
                 # Add items to the list - start row
                 csv_list.extend([reObj.group(1),  ',', reObj.group(2)])
+                mydict['time'] = reObj.group(1)
+                mydict['CPU'] = reObj.group(2)
                 # print('CPU={0}; time={1}'.format(
                 #     reObj.group(2).__str__(),
                 #     reObj.group(1).__str__()))
         else:
             # regex for function and percentage
-            # regex = r"\/([\w-]+)\s+\(\d+\)\s([\d.]+)%"
-            regex = r"\b(\d+:\d+:\d+).\d\s+\|[\s\d:]+HB:\s+[\b\w+\/]+\/([\w-]+)\s+\(\d+\)\s([\d.]+)%"
+            regex = r"\b(\d+:\d+:\d+)[.\d\s]+\|[\s\d:]+HB:\s+[\b\w+\/.]+\/([\w-]+)\s+\(\d+\)\s([\d.]+)%"
             reObj = re.search(regex, line)
-            # if reObj:
-            #     # Add items to the list
-            #     print('{0}; {1} -> {2}%'.format(
-            #         reObj.group(1).__str__(),
-            #         reObj.group(2).__str__(),
-            #         reObj.group(3).__str__()))
+            if reObj:
+                mydict[reObj.group(2).__str__()] = reObj.group(3)
+                # # Add items to the list
+                # print('{0}; {1} -> {2}%'.format(
+                #     reObj.group(1).__str__(),
+                #     reObj.group(2).__str__(),
+                #     reObj.group(3).__str__()))
 
 
 
@@ -184,8 +211,6 @@ def main():
     print("Log file: " + log_file_name + "\n")
     parseAll(log_file_name)
     findHeader(log_file_name)
-
-    # index(logFileName, patterns=('dump', 'ErrorDump', 'trigger'))
 
 
 if __name__ == '__main__':
