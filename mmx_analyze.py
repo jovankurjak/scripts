c10e84927cba079387f4e340214073dd3976d0e1
Jovan Kurjakov <kurjakdva@gmail.com>
2018-04-20 13:17:40 +0200

    Implement basic parse arg feature
    -added 'None' return if mmx file is not found
    -Removed global variables myList and myDict
    -Added comments to functions(not complete)

--- mmx_analyze.py
+++ mmx_analyze.py
@@ -8,7 +8,10 @@ import re
 import csv
 import collections
 
+# Program version
+prog_version = '0.1'
 
+# Importaint functions
 systemApplications = [
     'medialauncher',
     'smartphone_integrator',
@@ -29,39 +32,59 @@ systemApplications = [
     'systemservices'
 ]
 
-mydict = {}
-mylist = []
 
 
-# Use to get arguments if needed
 def getArgs():
-    parser = argparse.ArgumentParser(description='Search log for keywords')
-    parser.add_argument('pattern', action="store", type=str, nargs='+',
-                   help='pattern that is used for search through file')
-    parser.add_argument('-c'
-                        , nargs='?', help='show only the count of the provided pattern through file')
+    """This function will be used to parse args
 
+    Explanation of each option should be done here
+
+    Args:
+
+    Returns:
+
+    Todo:
+        * Add a workflow how to use parsed arguments
+        * Define few options and how to use them
+        * Define a default behaviour
+        * Update help
+    """
+    parser = argparse.ArgumentParser(description='MIB2P analyze tool')
+    parser.add_argument('-p',
+                        '--pattern',
+                        action="store",
+                        type=str,
+                        nargs='+',
+                        help='pattern that is used for search through file')
+    parser.add_argument('-c',
+                        '--cpu',
+                        action='store_true',
+                        default=False,
+                        help='make a CPU analysis of an MMX file in current folder')
+    parser.add_argument('-v', '--version', action='version', version='%(prog)s {0}'.format(prog_version))
+
+    # args = vars(parser.parse_args())
     args = parser.parse_args()
     print(args)
-    # print ("Starting script %s" % (argv[0]))
-    #
-    # print ("Number of arguments: ", len(argv))
-    # print ("The arguments are: ", str(argv))
+    print("print cpu={0}".format(args.cpu.__str__()))
+    print("print pattern={0}".format(args.pattern.__str__()))
+
+    return args
 
 
-# Search for log that contains MMX data(pattern 'HB:')
-# and return log file name
 def getMmxSerialFileName():
+    """Get file name of MMX serial log
+
+    Returns:
+        String: file name, None otherwise.
+
+    """
     files = [f for f in os.listdir('.') if os.path.isfile(f)]
     for f in files:
         if f.endswith(".txt"):
             if searchForPattern(f, 'HB:'):
                 return f.__str__()
-
-
-def parseAll(file):
-    with open(file) as fp:
-        print("Sum lines: " + len(fp.readlines()).__str__())
+    return None
 
 
 # searches for a certain pattern inside the log file
@@ -79,23 +102,26 @@ def searchForPattern(f, pattern):
 
 # find headers in provided file
 def findHeader(file):
-    """
+    """Parse MMX serial log file
+
     This function goes through MMX log file and parses every
     HB section for overall CPU consumption and CPU consumption of
     currently active processes at that time
     """
     tellPosition = 0
     headerCounter = 0
-    csv_list = []
+    mylist = []
+    my_dict = {}
     # csvfile = '/home/rtrk/Desktop/cpu.csv'
     csvfile = 'cpu_quick_analysis.csv'
     with open(file) as fp:
         fp.seek(0, 2)
         EOF = fp.tell()
-        print(EOF)
         fp.seek(0)
 
+        print("Sum lines: " + len(fp.readlines()).__str__())
         while(True):
+            """parse the file"""
             # Check for EOF
             if tellPosition == EOF:
                 break
@@ -107,10 +133,10 @@ def findHeader(file):
                 break
             headerCounter += 1
             tellPositionEnd = fp.tell()
-            parseForFunctions(fp, curHeaderStart, curHeaderEnd, csv_list)
+            (mylist, my_dict) = parseForFunctions(fp, curHeaderStart, curHeaderEnd, mylist, my_dict)
             tellPosition = fp.tell()
 
-        print('number of HB headers: ' + headerCounter.__str__() + " " + tellPosition.__str__())
+        print('number of HB headers: ' + headerCounter.__str__() + " end .tell() position:" + tellPosition.__str__())
 
         with open(csvfile, "w") as output:
             """Write the list to csv file."""
@@ -150,6 +176,11 @@ def findHeader(file):
 
 
 def getNextPattern(filePointer, lineNumber, pattern):
+    """Module function
+
+    This function searches for 'pattern' starting from 'lineNumber'
+    using file descriptor 'filePointer'.
+    """
     filePointer.seek(lineNumber)
     current_tell = filePointer.tell()
     for line_number, line in enumerate(iter(filePointer.readline, '')):
@@ -162,20 +193,28 @@ def getNextPattern(filePointer, lineNumber, pattern):
     return current_tell
 
 
-def parseForFunctions(filePointer, startPosition, endPosition, csv_list):
+def parseForFunctions(filePointer, startPosition, endPosition, my_list, my_dict):
+    """Module function
+
+    Args:
+        filePointer (file desc): file descriptor currently parsed object.
+        startPosition (int): from what position in file to start parsing.
+        endPosition (int): to what position in file to parse.
+        my_list (List): List containing rows of the CPU table.
+        my_dict (Dictionary): Dictionary containing values for 1 row of my_list.
+    """
     filePointer.seek(startPosition + 1)
     for line_number, line in enumerate(iter(filePointer.readline, '')):
         if filePointer.tell() == endPosition:
             # End list row
-            csv_list.append('\n')
-            od = collections.OrderedDict(sorted(mydict.items()))
-            mylist.append(od)
+            od = collections.OrderedDict(sorted(my_dict.items()))
+            my_list.append(od)
             # print(od)
             # print_od = ''
             # for key in od:
             #     print_od += '[' + key + ':' + od[key] + '%' + '] '
             # print(print_od)
-            for value in mydict.values():
+            for value in my_dict.values():
                 value = ''
             break
         line = line.rstrip()
@@ -185,9 +224,8 @@ def parseForFunctions(filePointer, startPosition, endPosition, csv_list):
             reObj = re.search(regex, line)
             if reObj:
                 # Add items to the list - start row
-                csv_list.extend([reObj.group(1),  ',', reObj.group(2)])
-                mydict['time'] = reObj.group(1)
-                mydict['CPU'] = reObj.group(2)
+                my_dict['time'] = reObj.group(1)
+                my_dict['CPU'] = reObj.group(2)
                 # print('CPU={0}; time={1}'.format(
                 #     reObj.group(2).__str__(),
                 #     reObj.group(1).__str__()))
@@ -196,21 +234,27 @@ def parseForFunctions(filePointer, startPosition, endPosition, csv_list):
             regex = r"\b(\d+:\d+:\d+)[.\d\s]+\|[\s\d:]+HB:\s+[\b\w+\/.]+\/([\w-]+)\s+\(\d+\)\s([\d.]+)%"
             reObj = re.search(regex, line)
             if reObj:
-                mydict[reObj.group(2).__str__()] = reObj.group(3)
+                my_dict[reObj.group(2).__str__()] = reObj.group(3)
                 # # Add items to the list
                 # print('{0}; {1} -> {2}%'.format(
                 #     reObj.group(1).__str__(),
                 #     reObj.group(2).__str__(),
                 #     reObj.group(3).__str__()))
+    return (my_list, my_dict)
 
 
 
 def main():
-    # args = getArgs()
+    args = getArgs()
     log_file_name = getMmxSerialFileName()
+    if log_file_name is None:
+        print('ERROR: file not found!')
+        return
     print("Log file: " + log_file_name + "\n")
-    parseAll(log_file_name)
-    findHeader(log_file_name)
+    if args.cpu is True:
+        findHeader(log_file_name)
+    else:
+        print('No option selected! use -h for help')
 
 
 if __name__ == '__main__':
